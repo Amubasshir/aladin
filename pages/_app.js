0e4cd142765f301a5639da3f908f07ac017e8d34
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-10 11:04:44 +0600

    initial setup redux scss mongo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../store';
+import '../styles/globals.scss';
+
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
