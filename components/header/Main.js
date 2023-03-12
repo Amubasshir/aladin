cc6c5d7d57542c171fa6c9b9975aef2dc9e2f613
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-12 21:10:25 +0600

    header full complete

--- components/header/Main.js
+++ components/header/Main.js
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import { FaOpencart } from 'react-icons/fa';
 import { RiSearch2Line } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
 import styles from './styles.module.scss';
 const Main = () => {
+  const { cart } = useSelector((state) => ({ ...state }));
   return (
     <div className={styles.main}>
       <div className={styles.main_container}>
         <Link legacyBehavior href="/">
           <a className={styles.logo}>
-            <img src="../../../aladin-text-logoo.png" alt="" />
+            <img src="https://i.ibb.co/qdcJDqJ/aladin-text-logoo.png" alt="" />
           </a>
         </Link>
         <div className={styles.search}>
@@ -20,6 +22,7 @@ const Main = () => {
         <Link legacyBehavior href="/cart">
           <a className={styles.cart}>
             <FaOpencart />
+            <span>0</span>
           </a>
         </Link>
       </div>
